f842e897a944a4c902da878d1d64f8bd75460d78
James Foster <james.foster@excelpoint.co.uk>
2012-08-05 16:26:52 +0100

    Add test project.

---
+++ source/CodeThoughts.Web.Tests/Controllers/BlogControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using CodeThoughts.Controllers;
+using NUnit.Framework;
+
+namespace CodeThoughts.Web.Tests.Controllers
+{
+	[TestFixture]
+	public class BlogControllerTests
+	{
+		[Test]
+		public void IndexReturnsViewResult()
+		{
+			var controller = new BlogController();
+
+			var result = controller.Index();
+
+			Assert.That(result, Is.AssignableFrom<ViewResult>());
+		}
+	}
+}
