197d31f9af647b6caa0995c0f2b0640e61dd1fe9
James Foster <james.foster@excelpoint.co.uk>
2012-08-05 03:41:10 +0100

    Fix multiple controllers error

--- source/CodeThoughts/Areas/Admin/AdminAreaRegistration.cs
+++ source/CodeThoughts/Areas/Admin/AdminAreaRegistration.cs
@@ -14,7 +14,8 @@
 			context.MapRoute(
 				"Admin_default",
 				"Admin/{controller}/{action}/{id}",
-				new {action = "Index", id = UrlParameter.Optional}
+				new {action = "Index", id = UrlParameter.Optional},
+				new[] {"CodeThoughts.Areas.Admin.Controllers"}
 				);
 		}
 	}
